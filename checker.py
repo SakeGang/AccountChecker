9b63e4bfce4adcbe9423ad61ae63331c3d781c59
Loac3 <78459141+Loac3@users.noreply.github.com>
2021-02-20 14:27:53 -0800

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,1 +1,22 @@
+import requests
+print("""
 
+
+ _____       _          _____ _               _             
+/  ___|     | |        /  __ \ |             | |            
+\ `--.  __ _| | _____  | /  \/ |__   ___  ___| | _____ _ __ 
+ `--. \/ _` | |/ / _ \ | |   | '_ \ / _ \/ __| |/ / _ \ '__|
+/\__/ / (_| |   <  __/ | \__/\ | | |  __/ (__|   <  __/ |   
+\____/ \__,_|_|\_\___|  \____/_| |_|\___|\___|_|\_\___|_|   
+                                                            
+                                                            
+""")
+print("""
+[1] Minecraft Checker
+[2] Spotify Checker
+[3] Gmail Checker
+[4] Uplay Checker
+[5] Steam Checker 
+[6] Origin Checker
+""")
+choice = input("Choice: ")
